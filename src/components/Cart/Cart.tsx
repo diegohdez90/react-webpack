ef43dc9621bc807d3412a599b6a4fe8920e5bf66
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2022-09-13 13:43:45 -0500

    feat: add counter items in cart component

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -4,6 +4,7 @@ import { Cart as CartIcon, Plus } from 'react-bootstrap-icons';
 import OverlayTrigger from 'react-bootstrap/esm/OverlayTrigger';
 import { AppContext } from '../../context/AppContext';
 import { AppContextValue } from '../../interfaces/AppContextValue';
+import { ZERO } from '../../utils/constants';
 
 interface Props {}
 
@@ -11,21 +12,24 @@ class Cart extends React.Component<Props> {
 
 	render() {
 		return (
-			<AppContext.Consumer>{(state: AppContextValue) =>
-				<OverlayTrigger
-					placement='bottom'
-					overlay={<Popover>
-						<Popover.Body>{
-							state.cart.items.length > 0 ? `${state.cart.items.length} pizza${state.cart.items.length > 1 ? 's': ''}` : 'No pizza selected'
-						}
-							<ul>
-								{state.cart.items.map((pizza, index) => (<li key={`${index}-${pizza.pizzaId}`}>${pizza.name} {pizza.quantity > 1 ? (<React.Fragment><Plus /> {pizza.quantity}</React.Fragment>) : null}</li>))}
-							</ul>
-						</Popover.Body>
-					</Popover>}
-				>
-					<button className="btn btn-link text-decoration-none"><CartIcon /> {state.cart.items.length} added</button>
-				</OverlayTrigger>
+			<AppContext.Consumer>{
+				(state: AppContextValue) => {
+					const total = state.cart.items.reduce((sum, item) => item.quantity + sum, ZERO)
+					return (<OverlayTrigger
+						placement='bottom'
+						overlay={<Popover>
+							<Popover.Body>
+								<ul>
+									{state.cart.items.map((pizza, index) => (<li key={`${index}-${pizza.pizzaId}`}>${pizza.name} {pizza.quantity > 1 ? (<React.Fragment><Plus /> {pizza.quantity}</React.Fragment>) : null}</li>))}
+								</ul>
+							</Popover.Body>
+						</Popover>}
+					>
+						<button className="btn btn-link text-decoration-none"><CartIcon />{
+								total > 0 ? `${total} pizza${total > 1 ? 's': ''}` : 'No pizza selected'
+							}</button>
+					</OverlayTrigger>)
+				}
 			}</AppContext.Consumer>
 		);
 	}
